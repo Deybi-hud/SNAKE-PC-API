fdc04b6e433b8b4bf8db432abbaece1df0d78c82
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-11 04:40:05 -0300

    sd

--- src/main/java/SNAKE_PC/demo/repository/usuario/ContactoRepository.java
+++ src/main/java/SNAKE_PC/demo/repository/usuario/ContactoRepository.java
@@ -1,12 +1,14 @@
 package SNAKE_PC.demo.repository.usuario;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import SNAKE_PC.demo.model.usuario.Contacto;
 
 public interface ContactoRepository extends JpaRepository<Contacto, Long> {
 
-    
-
+    boolean existsByTelefono(Integer id);
+        
     
 }
