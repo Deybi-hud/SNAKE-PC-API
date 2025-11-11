c1aa34ca7814c97eca09dfacd53bd254dd583d0d
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-11 04:30:10 -0300

    sad

--- src/main/java/SNAKE_PC/demo/repository/usuario/ComunaRepository.java
+++ src/main/java/SNAKE_PC/demo/repository/usuario/ComunaRepository.java
@@ -1,11 +1,14 @@
 package SNAKE_PC.demo.repository.usuario;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import SNAKE_PC.demo.model.usuario.Comuna;
 
 public interface ComunaRepository extends JpaRepository<Comuna, Long> {
 
-    
+    Optional<Comuna> findByNombreComuna(String nombreComuna);
+    boolean existsByNombreComuna(String nombreComuna);
     
 }
