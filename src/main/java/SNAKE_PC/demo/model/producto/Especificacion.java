1fb7421f7e636715b93a606a9491bd048bd2465c
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-11-23 03:10:15 -0300

    OK

---
+++ src/main/java/SNAKE_PC/demo/model/producto/Especificacion.java
@@ -0,0 +1,42 @@
+package SNAKE_PC.demo.model.producto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "especificacion")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Especificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "frecuencia", nullable = true)
+    private String frecuencia;
+
+    @Column(name = "capacidad", nullable = true)
+    private String capacidadAlmacenamiento;
+
+    @Column(name = "consumo", nullable = true)
+    private String consumo;
+
+    @OneToMany(mappedBy = "especificacion")
+    @JsonIgnore
+    private List<Producto> productos;
+}
---
+++ src/main/java/SNAKE_PC/demo/model/producto/Imagen.java
@@ -0,0 +1,33 @@
+package SNAKE_PC.demo.model.producto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "imagen")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Imagen {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url", nullable = false)
+    private String imagenUrl;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    private Producto producto;
+
+}
