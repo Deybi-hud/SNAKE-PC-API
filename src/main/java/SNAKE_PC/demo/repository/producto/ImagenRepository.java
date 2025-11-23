7e63f855c437b451af2aa1e33d5c763aa3a772be
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-22 22:52:36 -0300

    asd

--- src/main/java/SNAKE_PC/demo/repository/producto/ImagenRepository.java
+++ src/main/java/SNAKE_PC/demo/repository/producto/ImagenRepository.java
@@ -1,5 +1,7 @@
 package SNAKE_PC.demo.repository.producto;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import SNAKE_PC.demo.model.producto.Imagen;
 
 @Repository
 public interface ImagenRepository extends JpaRepository<Imagen, Long> {
+    
+    List<Imagen> findByProductoId(Long productoId);
 
+    long deleteByProductoId(Long productoId);
 }
